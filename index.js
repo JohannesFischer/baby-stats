13aef5185c9af07c3c65d8e5e6663f0a0f55dce0
Johannes Fischer <johannes.fischer@rakuten.com>
2018-01-26 13:41:35 +0900

    add moment lib

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-console.log('it works!');
-
+const moment = require('moment');
 const csvr = require('./csv-reader');
 
 csvr.test().then(data => {
-  console.log(data);
+  // console.log(data);
+
+  for (row of data) {
+    const [ baby, time, duration, note ] = row;
+
+    const date = moment(time,'DD/MM/YYYY HHmm').format();
+    console.log(date);
+  }
 });
+
+
